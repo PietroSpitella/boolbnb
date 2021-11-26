2630aaf89fb44ec60b4ca96045e731b78eee92e4
Luciano <lucco6823@gmail.com>
2021-11-26 18:21:14 +0100

    Continue work on details apartments

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -19,18 +19,34 @@
                 <img class="apartments-img" src="{{$apartment->image}}" alt="Image inserted">
             </div>
             <p class="fs-25 font-weight-bold">{{$apartment->title}}</p>
-            <i class="fas fa-map-marker-alt fs-25"></i>
-            <p> {{$apartment->city}}, {{$apartment->street}}, {{$apartment->house_number}}.</p>
-            <p>type: {{$apartment->type}} </p>
-            <p>description: {{$apartment->description}}</p>
-            <p>mq: {{$apartment->mq}}</p>
-            <p>n_beds: {{$apartment->n_beds}}</p>
-            <p>n_guests: {{$apartment->n_guests}}</p>
+            <p class="fs-15"><i class="fas fa-map-marker-alt fs-15"></i> {{$apartment->city}}, {{$apartment->street}}, {{$apartment->house_number}}</p>
+            
+            <div class="div-bordered text-center mt-3">
+                <div class="p-4">
+                    <i class="fas fa-home fs-30 color-grey-icon"></i>
+                    <p class="fs-15 text-capitalize font-weight-bold">Type {{$apartment->type}}</p>
+                </div>
+            </div>
+
+            <div class="div-bordered d-flex text-center mb-3">
+                    <div class="div-bordered-2 div-bordered-3 div-bordered-4 icon-separation p-4">
+                        <i class="fas fa-bed fs-20 color-grey-icon"></i>
+                        <p class="fs-15 text-capitalize font-weight-bold">Bedrooms {{$apartment->n_beds}}</p>
+                    </div>
+                    <div class="div-bordered-3 icon-separation p-4">
+                        <i class="fas fa-user-friends fs-20 color-grey-icon"></i>
+                        <p class="fs-15 text-capitalize font-weight-bold">Guests {{$apartment->n_guests}}</p>
+                    </div>
+                    <div class="div-bordered-2 div-bordered-3 div-bordered-4 icon-separation p-4">
+                        <i class="far fa-clone fs-20 color-grey-icon"></i>
+                        <p class="fs-15 text-capitalize font-weight-bold">Mq {{$apartment->mq}}</p>
+                    </div>
+            </div>
+            <p>Description: {{$apartment->description}}</p>
             <p>pet: {{$apartment->pet}}</p>
             <p>h_checkin: {{$apartment->h_checkin}}</p>
             <p>h_checkout: {{$apartment->h_checkout}}</p>
             <p>price_night: {{$apartment->price_night}}</p>
-            <p>image: {{$apartment->image}}</p>
             <p>visibility: {{$apartment->visibility}}</p>
             <p>city: {{$apartment->city}}</p>
             <p>street: {{$apartment->street}}</p>
