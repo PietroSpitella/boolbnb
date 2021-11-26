3cfe130c82b6fd6d368db7d9868c07e0e45f6910
amedeo <amedeopasanisi97@gmail.com>
2021-11-26 17:10:43 +0100

    creazione spazio cards appartamenti e footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,45 @@
+{{-- FOOTER index guest --}}
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h2>sezione
+            </h2>
+            <ul>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+            </ul>
+        </div>
+        <div class="col">
+            <h2>sezione
+            </h2>
+            <ul>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+            </ul>
+        </div>
+        <div class="col">
+            <h2>sezione
+            </h2>
+            <ul>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+            </ul>
+        </div>
+        <div class="col">
+            <h2>sezione
+            </h2>
+            <ul>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+                <li>testo</li>
+            </ul>
+        </div>
+    </div>
+</div>
