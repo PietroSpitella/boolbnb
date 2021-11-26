34c486a1043b5baf3f70894760eea88b811dd7e1
amedeo <amedeopasanisi97@gmail.com>
2021-11-26 15:50:46 +0100

    inizio layout home

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/albertogaia/Desktop/boolbnb/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/albertogaia/Desktop/boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\AMEDEO\Percorso di studi\Boolean\LEZIONI\95 progetto finale\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\AMEDEO\Percorso di studi\Boolean\LEZIONI\95 progetto finale\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
