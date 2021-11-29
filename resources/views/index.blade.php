dd3720a22b6b0ed2971313fa90f31822765ab553
amedeo <amedeopasanisi97@gmail.com>
2021-11-29 17:27:16 +0100

    bozza cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     {{-- Immagine --}}
     <div class="img-overlay position-absolute"></div>
     <img
-        class="img-fluid position-absolute"
+        class="img-fluid position-absolute w-100"
         src="https://www.greenme.it/wp-content/uploads/2021/02/tiny-house-ikea.jpg"
         alt=""
     >
@@ -18,7 +18,7 @@
         {{-- Titoli --}}
         <div class="row align-items-center flex-column mb-4">
             <div class="col-auto mx-auto text-center">
-                <h2 class="mb-4">Book & Experience Amazing Places</h2>
+                <h2 class="mb-4"><strong>Book & Experience Amazing Places</strong></h2>
                 <h5 class="mb-0">Theme For Booking and Rental</h5>
             </div>
         </div>
@@ -52,15 +52,15 @@
 {{-- sezione2: Sponsored --}}
 <section id="sponsored" class="">
     <div class="container">
-        <div class="row">
+        {{-- Titoli --}}
+        <div class="row mb-3">
             <div class="col-12">
-                <h2>Our Featured Homes</h2>
-            </div>
-            <div class="col-12">
-                <h3>Hand-picked selection of quality places</h3>
+                <h3 class="mb-3"><strong>Our Featured Homes</strong></h3>
+                <h5 class="mb-0">Hand-picked selection of quality places</h5>
             </div>
         </div>
-    
+        
+        {{-- Bottoni Prev, Next --}}
         <div class="row justify-content-end mb-3">
             <button type="button" class="btn btn-outline-danger btn-sm px-1 py-0 mr-2">Prev</button>
             <button type="button" class="btn btn-outline-danger btn-sm px-1 py-0">Next</button>
@@ -69,16 +69,37 @@
         {{-- Cards degli appartamenti sponsorizzati --}}
         <div class="row">
             <div class="col-4">
-                <h2>CARD</h2>
+                <div class="card">
+                    <img src="https://www.greenme.it/wp-content/uploads/2021/02/tiny-house-ikea.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                    </div>
+                </div>
             </div>
             <div class="col-4">
-                <h2>CARD</h2>
+                <div class="card">
+                    <img src="https://www.greenme.it/wp-content/uploads/2021/02/tiny-house-ikea.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                    </div>
+                </div>
             </div>
             <div class="col-4">
-                <h2>CARD</h2>
-            </div>
-            <div class="col-4">
-                <h2>CARD</h2>
+                <div class="card">
+                    <img src="https://www.greenme.it/wp-content/uploads/2021/02/tiny-house-ikea.jpg" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
