d2ec58176d92a7e058d83d71d5251ad027fab517
amedeo <amedeopasanisi97@gmail.com>
2021-11-29 16:29:22 +0100

    bozza 1 jumbotron

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,39 +1,19 @@
 {{-- FOOTER index guest --}}
-<div class="container">
+<div class="container pt-5">
     <div class="row">
         <div class="col">
-            <h2>sezione
-            </h2>
-            <ul>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-            </ul>
+            <address>
+                <h2>Sezione footer</h2>
+                <ul>
+                    <li>testo</li>
+                    <li>testo</li>
+                    <li>testo</li>
+                    <li>testo</li>
+                </ul>
+            </address>
         </div>
         <div class="col">
-            <h2>sezione
-            </h2>
-            <ul>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-            </ul>
-        </div>
-        <div class="col">
-            <h2>sezione
-            </h2>
-            <ul>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-            </ul>
-        </div>
-        <div class="col">
-            <h2>sezione
-            </h2>
+            <h2>Sezione footer</h2>
             <ul>
                 <li>testo</li>
                 <li>testo</li>
