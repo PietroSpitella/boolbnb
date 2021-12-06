94aaef4d78c5cbd0d912e2bf053d4c5a6cf94d3e
amedeo <amedeopasanisi97@gmail.com>
2021-12-06 11:04:41 +0100

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Apartment;
+use App\Service;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
@@ -24,7 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('index');
+        $services = Service::all();
+        $apartments = Apartment::all();
+        return view('index', compact('apartments', 'services'));
     }
 
     public function about(){
