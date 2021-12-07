0d1fcefeef6f4e2be96f52c2f209f68475dbff12
albertogaia <alberto.gaia95@gmail.com>
2021-12-07 15:02:48 +0100

    added map at discovery

--- resources/js/pages/Discover.vue
+++ resources/js/pages/Discover.vue
@@ -1,101 +1,114 @@
 <template>
-  <div v-if="!isLoading" class="container my-3">
-    {{ destination }}
-    <div class="form-group">
-      <input
-        type="text"
-        v-model="city"
-        @keyup.enter="getApartments"
-        placeholder="Città"
-        class="form-control"
-      />
-      <div id="map_div" class="map"></div>
-      <button
-        class="btn btn-primary my-3"
-        @click="getApartments"
-        id="getCityBtn"
-      >
-        Vai
-      </button>
-    </div>
-    <template>
-      <div v-if="citySearched">
-        <div class="form-services row">
-          <div
-            class="form-group col-2"
-            v-for="service in services"
-            :key="service.id"
-          >
-            <label :for="service.id">{{ service.name }}</label>
+  <div>
+    <div class="container my-3">
+      <div class="form-group">
+        <input
+          type="text"
+          v-model="city"
+          @keyup.enter="getApartments"
+          placeholder="Città"
+          class="form-control"
+        />
+        <button
+          class="btn btn-primary my-3"
+          @click="getApartments"
+          id="getCityBtn"
+        >
+          Vai
+        </button>
+      </div>
+      <template>
+        <div v-if="citySearched">
+          <div class="form-services row">
+            <div
+              class="form-group col-2"
+              v-for="service in services"
+              :key="service.id"
+            >
+              <label :for="service.id">{{ service.name }}</label>
+              <input
+                type="checkbox"
+                :name="service.name"
+                :id="service.id"
+                class=""
+                :value="service.id"
+                @change="getSelectedServices"
+              />
+            </div>
+          </div>
+          <div class="form-group">
             <input
-              type="checkbox"
-              :name="service.name"
-              :id="service.id"
-              class=""
-              :value="service.id"
-              @change="getSelectedServices"
+              class="form-control"
+              type="number"
+              v-model="guests"
+              placeholder="Numero ospiti"
+              @change="getApartments"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              class="form-control"
+              type="number"
+              v-model="rooms"
+              placeholder="Numero stanze"
+              @change="getApartments"
+            />
+          </div>
+          <div class="form-group">
+            <label class="" for="rangeDistance">Raggio: {{ distance }}Km</label>
+            <input
+              class="form-control"
+              type="range"
+              v-model="distance"
+              placeholder="Distanza"
+              @change="getApartments"
+              max="40"
+              id="rangeDistance"
             />
           </div>
         </div>
-        <div class="form-group">
-          <input
-            class="form-control"
-            type="number"
-            v-model="guests"
-            placeholder="Numero ospiti"
-            @change="getApartments"
-          />
-        </div>
-        <div class="form-group">
-          <input
-            class="form-control"
-            type="number"
-            v-model="rooms"
-            placeholder="Numero stanze"
-            @change="getApartments"
-          />
-        </div>
-        <div class="form-group">
-          <label class="" for="rangeDistance">Raggio: {{ distance }}Km</label>
-          <input
-            class="form-control"
-            type="range"
-            v-model="distance"
-            placeholder="Distanza"
-            @change="getApartments"
-            max="40"
-            id="rangeDistance"
-          />
+      </template>
+    </div>
+    <div class="row justify-content-around">
+      <div class="col-12 col-md-3">
+        <div class="row" v-for="(apartment, index) in apartments" :key="index">
+          <div class="col-12">
+            <div class="card mb-3" style="max-width: 540px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img
+                    :src="'/storage/' + apartment.image"
+                    class="card-img-top"
+                    alt=""
+                  />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ apartment.title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      {{ apartment.city }}
+                    </h6>
+                    <p class="card-text">{{ apartment.description }}</p>
+                    <p class="card-text">
+                      <router-link
+                        :to="{
+                          name: 'Apartment',
+                          params: { slug: apartment.slug },
+                        }"
+                        class="card-link"
+                        >Visualizza
+                      </router-link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </template>
-    <div class="row mt-4">
-      <div
-        class="col-lg-4 mb-4"
-        v-for="(apartment, index) in apartments"
-        :key="index"
-      >
-        <div class="card">
-          <img
-            :src="'/storage/' + apartment.image"
-            class="card-img-top"
-            alt=""
-          />
-          <div class="card-body">
-            <h5 class="card-title">{{ apartment.title }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">
-              {{ apartment.city }}
-            </h6>
-            <p class="card-text">{{ apartment.description }}</p>
-            <router-link
-              :to="{
-                name: 'Apartment',
-                params: { slug: apartment.slug },
-              }"
-              class="card-link"
-              >Visualizza
-            </router-link>
-          </div>
+      <div class="col-12 col-md-8">
+        <div class="container-map">
+          <div id="map_div" class="map"></div>
         </div>
       </div>
     </div>
@@ -116,8 +129,8 @@ export default {
       apiKey: ".json?key=6pyK2YdKNiLrHrARYvnllho6iAdjMPex",
       API_KEY: "6pyK2YdKNiLrHrARYvnllho6iAdjMPex",
       apartments: [],
-      lat: "",
-      long: "",
+      lat: "41.89193",
+      long: "12.51133",
       citySearched: false,
       services: [],
       selectedServices: [],
@@ -128,8 +141,6 @@ export default {
       popupOffsets: {
         top: [0, 0],
         bottom: [0, -70],
-        "bottom-right": [0, -70],
-        "bottom-left": [0, -70],
         left: [25, -35],
         right: [-25, -35],
       },
@@ -147,6 +158,7 @@ export default {
           .then((res) => {
             this.lat = res.data.results[0].position.lat;
             this.long = res.data.results[0].position.lon;
+            this.citySearched = true;
           });
       }
       axios
@@ -169,12 +181,12 @@ export default {
         )
         .then((res) => {
           this.apartments = res.data.results;
-          // this.createMarker(this.apartments);
-          this.isLoading = false;
+          this.createMarker(this.apartments);
+          // this.isLoading = false;
         });
-      // if (this.map != undefined) {
-      //   this.getMap();
-      // }
+      if (this.map != undefined) {
+        this.getMap();
+      }
     },
     getSelectedServices(el) {
       if (el.target.checked === true) {
@@ -187,40 +199,50 @@ export default {
       }
       this.getApartments();
     },
-    // getMap() {
-    //   this.map = tt.map({
-    //     container: "map_div",
-    //     key: this.API_KEY,
-    //     source: "vector",
-    //     center: [this.long, this.lat],
-    //     zoom: 12,
-    //   });
-    //   this.map.addControl(new tt.FullscreenControl());
-    //   this.map.addControl(new tt.NavigationControl());
-    //   console.log(this.map);
-    // },
-    // createMarker(arr) {
-    //   arr.forEach((element) => {
-    //     let cor = [element.long, element.lat];
-    //     let marker = new tt.Marker().setLngLat(cor).addTo(this.map);
-    //     let popup = new tt.Popup({ offset: this.popupOffsets }).setHTML(
-    //       `${element.name}`
-    //     );
-    //     marker.setPopup(popup);
-    //   });
-    // },
+    getMap() {
+      this.map = tt.map({
+        container: "map_div",
+        key: this.API_KEY,
+        source: "vector",
+        center: [this.long, this.lat],
+        zoom: 14,
+      });
+      this.map.addControl(new tt.FullscreenControl());
+      this.map.addControl(new tt.NavigationControl());
+    },
+    createMarker(arr) {
+      arr.forEach((element) => {
+        let cor = [element.long, element.lat];
+        let marker = new tt.Marker().setLngLat(cor).addTo(this.map);
+        let popup = new tt.Popup({ offset: this.popupOffsets }).setHTML(
+          `<div class="card-body p-1">
+              <h5 class="card-title">${element.title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">
+                ${element.type}
+              </h6>
+              <p class="card-text">${element.street}</p>
+            </div>`
+        );
+        marker.setPopup(popup);
+      });
+    },
   },
   created() {
     this.getApartments();
     this.getServices();
   },
   mounted() {
-    // this.getMap();
+    this.getMap();
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.card-img-top {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 .marker-icon {
   background-position: center;
   background-size: 22px 22px;
@@ -257,8 +279,13 @@ export default {
   position: absolute;
   width: 24px;
 }
-#my-map {
-  width: 100%;
+
+.container-map {
   height: 100%;
+  min-height: 350px;
+  #map_div {
+    height: 100%;
+    overflow: hidden;
+  }
 }
 </style>
--- resources/views/guest/homepage.blade.php
+++ resources/views/guest/homepage.blade.php
@@ -14,8 +14,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps/maps.css'>
-
+    <link rel='stylesheet' type='text/css' href='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/maps/maps.css'>    
     <script src='https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.16.0/maps/maps-web.min.js'></script>
     <script src="https://api.tomtom.com/maps-sdk-for-web/cdn/6.x/6.15.0/services/services-web.min.js"></script>
 
