1e6bc43a92a111d07c69f608081cab0760407567
Luciano <lucco6823@gmail.com>
2021-12-07 11:36:52 +0100

    Complete_footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,30 @@
 {{-- FOOTER index guest --}}
-<div class="container pt-5">
-    <div class="row">
-        <div class="col">
-            <address>
-                <h2>Sezione footer</h2>
-                <ul>
-                    <li>testo</li>
-                    <li>testo</li>
-                    <li>testo</li>
-                    <li>testo</li>
+<div class="back-color-black d-flex justify-content-around pt-5">
+    <div class="container">
+        <div class="row d-flex">
+            <div class="col text-center">
+                <a href="./"><img src="images/boolbnb-def.png" alt="logo"></a>
+            </div>
+            <div class="col text-center">
+                <h2>Site creators</h2>
+                <ul class="list-unstyled">
+                    <li><a class="color-white" href="https://www.linkedin.com/in/alberto-gaia/">Alberto Gaia</li>
+                    <li><a class="color-white" href="https://www.linkedin.com/in/amedeo-pasanisi-88a331185/">Amedeo Pasanisi</li>
+                    <li><a class="color-white" href="https://www.linkedin.com/in/luciano-marchionna/">Luciano Marchionna</a></li>
+                    <li><a class="color-white" href="https://www.linkedin.com/in/pietro-spitella/">Pietro Spitella</li>
+                    <li><a class="color-white" href="https://www.linkedin.com/in/roberto-martino-86a301163/">Roberto Martino</li>
                 </ul>
-            </address>
-        </div>
-        <div class="col">
-            <h2>Sezione footer</h2>
-            <ul>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-                <li>testo</li>
-            </ul>
+            </div>
+            <div class="col text-center">
+                <h2>Other informationr</h2>
+                <ul class="list-unstyled">
+                    <li><a class="color-white" href="./">Home</li>
+                    <li><a class="color-white" href="./discover">Discover</li>
+                    <li><a class="color-white" href="./about-us">About Us</li>
+                    <li><a class="color-white" href="./host/dashboard">Dashboard</li>
+                    <li><a class="color-white" href="./host/messages">Messages</li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
